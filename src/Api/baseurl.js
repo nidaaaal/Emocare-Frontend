d72e2adaab21ae42fe96abd46e35286a589b8e59
nidal <nidalcheruvadi@gmail.com>
2025-11-22 11:56:09 +0530

    new url

--- src/Api/baseurl.js
+++ src/Api/baseurl.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://emocare-backend-uavk.onrender.com/api',
+  baseURL: 'https://emocare-backend-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
