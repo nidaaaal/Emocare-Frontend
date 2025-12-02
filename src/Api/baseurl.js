8837683f45ed4157f68bf1ce186d553dda9e3709
nidal <nidalcheruvadi@gmail.com>
2025-12-02 22:38:10 +0530

    route issue fixed

--- src/Api/baseurl.js
+++ src/Api/baseurl.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://emocare-backend-production.up.railway.app/api',
+  baseURL: 'https://localhost:7237/api', // || https://emocare-backend-production.up.railway.app
   headers: {
     'Content-Type': 'application/json',
   },
